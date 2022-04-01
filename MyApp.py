198a54bb7b828aacff4ebc0e82d44ff794b120d2
IsabelResendiz <102832238+IsabelResendiz@users.noreply.github.com>
2022-04-01 14:54:13 -0700

    k

--- MyApp.py
+++ MyApp.py
@@ -1,8 +1,9 @@
 import streamlit as st
 st.title("MI PRIMER APP")
 click=st.button("Dale click")
-st.write("el valor de click es: ",click")
-
+st.write("el valor de click es: ",click)
+if click==True:
+    st.image("https://media.istockphoto.com/photos/maltese-dog-puppy-picture-id961585286?k=20&m=961585286&s=612x612&w=0&h=9pNSBbt1WWTUXRHqzT57FEfh7WoAuWFGIqgLNikwty0=")
 import pandas as pd 
 
 #Las siguientes tres lineas eran del primer ejercicio
