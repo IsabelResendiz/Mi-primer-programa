b16405304117181462dec320302db5c2e48f56bf
IsabelResendiz <102832238+IsabelResendiz@users.noreply.github.com>
2022-04-01 15:11:12 -0700

    l

--- MyApp.py
+++ MyApp.py
@@ -19,7 +19,7 @@ st.markdown(" **Esta es una viñeta** ")
 num1 = st.slider("Elige el numero 1",0.0,100.0,25.0)
 num2 = st.slider("Elige el numero 2",0.0,100.0,25.0)
 suma = nu1+num2
-st.write("La suma de",num1,"y",num2,"es:",suma)
+st.write("La suma de",num1," y ",num2,"es:",suma)
 
 st.write("Ahora multipliquemos")
 nn1 = st.number_imput("Dame n1")
