a23cddd4b41e7c18e57b450acedbd52a6027bc21
IsabelResendiz <102832238+IsabelResendiz@users.noreply.github.com>
2022-04-01 14:35:18 -0700

    f

--- MyApp.py
+++ MyApp.py
@@ -3,7 +3,8 @@ st.title("MI PRIMER APP")
 #st.button("Dale click")
 #st.button("Otro botón")
 import pandas as pd 
-Las siguientes tres lineas 
+
+#Las siguientes tres lineas eran del primer ejercicio
 #df = pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
 #st.write(df)
 #st.map(df)
